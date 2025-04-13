e02a660656744072fadcb45c679cfc81913a468e
tirthankarm2006 <70090862+tirthankarm2006@users.noreply.github.com>
2025-04-13 23:01:18 +0530

    Added some files

---
+++ main2.hpp
@@ -0,0 +1,14 @@
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+#include<btBulletDynamicsCommon.h>
+#include<gtc/matrix_transform.hpp>
+#include<gtc/type_ptr.hpp>
+#include<glm.hpp>
+#include<assimp/camera.h>
+#include<assimp/mesh.h>
+#include<assimp/config.h>
+#include "stb_image.h"
+#include "imgui.h"
+//#include "imgui_impl_glfw.h"
+//#include "imgui_impl_opengl3.h"
+//#include "imgui_impl_opengl3_loader.h"
