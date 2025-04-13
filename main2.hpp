4a7cea4e8cdb8e124c2ec76fd4fa2a738c6ffb56
tirthankarm2006 <70090862+tirthankarm2006@users.noreply.github.com>
2025-04-14 02:42:58 +0530

    Added OpenAL

--- main2.hpp
+++ main2.hpp
@@ -9,6 +9,7 @@
 #include<assimp/config.h>
 #include "stb_image.h"
 #include "imgui.h"
+#include<al.h>
 //#include "imgui_impl_glfw.h"
 //#include "imgui_impl_opengl3.h"
 //#include "imgui_impl_opengl3_loader.h"
